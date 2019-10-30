8ef35cae5e843f06a02bf9daa78d2e448cbf3fe6
Aurélien Ravier <aurelien.ravier@unflux.fr>
2019-10-30 15:41:28 +0100

    Update unflux.js

--- js/unflux.js
+++ js/unflux.js
@@ -4,8 +4,47 @@
 
       init : function(){
 
+          this.agrimalin();
           this.popin();
-          //this.quiz();
+          this.quiz();
+      },
+
+      agrimalin : function(){
+
+        const quotes = [
+          [
+              "Wikipsum wishful wizards want wands Thomas Hyndeman DD (a.k.a. Hendeman or Hendyman) was an English medieval churchman"
+          ],
+          [
+              "The red-throated piping guan (Pipile cujubi) is a species of bird in the family Cracidae."
+          ],
+          [
+              "The heats were held at 10:00.[5] The semifinals were held at 18:18.[6] The final was held at 18:41.[7]"
+          ],
+          [
+            "Trogonophidae (Palearctic worm lizards or desert ringed lizards) is a small family of amphisbaenians, containing five species in four genera."
+          ],
+          [
+            "he Massachusetts Teachers' Oath was a loyalty oath required to teach in Massachusetts from 1935 to 1967."
+          ]
+      ];
+      const quote = document.querySelector('.quote');
+
+      let random = null;
+      random =  Math.floor(Math.random() * quotes.length);
+      quote.innerHTML = quotes[random];
+
+
+      //génération des anecdotes au clic du bouton 
+      const btnQuote = document.querySelector("#new-anecdote");
+      function newQuote() {
+        random =  Math.floor(Math.random() * quotes.length);
+        quote.innerHTML = quotes[random];
+      }
+
+      btnQuote.addEventListener('click', newQuote);
+
+
 
       },
 
@@ -50,7 +89,9 @@
 
 
 
-      /*quiz : function(){
+
+
+      quiz : function(){
         var quiz = {
           questions: [
             {
@@ -87,7 +128,7 @@
                 }
               ]
             },
-            {
+            /*{
               question: '3. Quelle émission préférez-vous ?',
               responses: [
                 {
@@ -188,7 +229,7 @@
                   value: 'parcours-animaux'
                 }
               ]
-            }
+            }*/
           ]
         };
 
@@ -261,8 +302,9 @@
             },
           }
         });
+      },
 
-      }*/
+
 
   }
   siaJS.init();
